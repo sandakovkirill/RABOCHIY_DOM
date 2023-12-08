f4e1c01bb17947e033391f907fd7057542532775
sandakovkirill <129473700+sandakovkirill@users.noreply.github.com>
2023-12-08 21:31:20 +0700

    Delete src directory

--- src/js/carousel.js
+++
@@ -1,8 +0,0 @@
-let buttonCarousel = document.getElementById('href-carousel');
-let images = ['slider-1.jpg', 'slider-2.jpg', 'slider-3.jpg'];
-let currentIndex = 1;
-
-buttonCarousel.addEventListener('click', function(){
-    document.getElementById('slider-img').src = 'img/' + images[currentIndex];
-    currentIndex = (currentIndex + 1) % images.length;
-});
--- src/js/hamburger_menu.js
+++
@@ -1,4 +0,0 @@
-let buttonHamburger = document.getElementsByClassName('button-hamburger')[0];
-buttonHamburger.addEventListener('click', function() {
-    document.querySelector('.menu').classList.toggle('show');
-});
